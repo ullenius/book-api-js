512e1674dae750667bb7a72e2bb701590ef36ca2
Anosh D. Ullenius <anosh@anosh.se>
2020-08-28 10:48:52 +0200

    View operation and parse response

--- book.js
+++ book.js
@@ -1,24 +1,80 @@
 "use strict";
 
-const BASE = "https://www.forverkliga.se/JavaScript/api/crud.php?requestKey";
+const BASE = "https://www.forverkliga.se/JavaScript/api/crud.php?";
+const requestKeyUrl = `${BASE}requestKey`;
 var key;
 
 function init() {
 
     var app = document.getElementById("app");
-    var key = localStorage.getItem("book-api");
+    var button = document.getElementById("knapp");
+    button.addEventListener("click", viewData);
+
+    key = localStorage.getItem("book-api");
 
     if (!key) {
         key = fetchKey();
     } else {
         console.log(`Cached key: ${key}`);
+        console.log("view url", viewUrl());
     }
+
+}
+
+function viewUrl() {
+
+    var base = authUrl();
+    return `${base}&op=select`;
+}
+
+function authUrl() {
+    return `${BASE}key=${key}`;
+}
+
+
+function viewData() {
+
+    fetch(viewRequest)
+
+    .then( function parseJSON(response) {
+
+        return response.json();
+    })
+
+    .then(function printData(data) {
+
+        var {
+            status,
+            message,
+            data : [
+                    {
+                        id,
+                        title,
+                        author,
+                        updated
+                    } = {}
+             ] = []
+        } = data;
+
+        if (status === "success") {
+
+            console.log(`id: ${id}`);
+            console.log(`title: ${title}`);
+            console.log(`author: ${author}`);
+            console.log(`updated: ${updated}`);
+        } else {
+
+            console.log("Failed! ", status);
+            console.log("Message: ", message);
+        }
+
+    });
 
 }
 
 function fetchKey() {
 
-    fetch(BASE)
+    fetch(requestKeyUrl)
 
     .then( function parseJSON(response) {
 
