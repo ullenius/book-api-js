1c116d6e55c23045ea01b3f59ffbdf93d963527a
Anosh D. Ullenius <anosh@anosh.se>
2020-08-28 10:11:41 +0200

    Save key in localStorage

--- book.js
+++ book.js
@@ -6,6 +6,17 @@ var key;
 function init() {
 
     var app = document.getElementById("app");
+    var key = localStorage.getItem("book-api");
+
+    if (!key) {
+        key = fetchKey();
+    } else {
+        console.log(`Cached key: ${key}`);
+    }
+
+}
+
+function fetchKey() {
 
     fetch(BASE)
 
@@ -15,9 +26,20 @@ function init() {
 
     }).then(function print(data) {
 
-        console.log(data);
+        var {
+            status,
+            key
+        } = data;
+
+        console.log("status ", status);
+        if (status === "success") {
+            localStorage.setItem("book-api", key);
+            console.log(key);
+            return key;
+        }
     });
 
 }
+
 
 window.onload = init;
